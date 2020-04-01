be1b40c5c2b520675db964bf5905c9a032eaa33b
Wander Silva <vanderlei.lopes@seredesa.com.br>
2020-04-01 00:05:13 -0300

    Front-end do app

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -3,6 +3,7 @@ const conection = require('../database/connection');
 module.exports = {
     async index(request, response) {
         const ong_id = request.headers.authorization;
+        console.log(ong_id);
 
         const incidents = await conection('incidents')
             .where('ong_id', ong_id)
